19f14d5d99327dc299e1cb356788ceeda499a10e
TUMIT_HASAN <tumithasan1@gmail.com>
2025-06-26 07:09:38 +0600

    server connect with mongodb

--- index.js
+++ index.js
@@ -2,22 +2,54 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-const PORT = process.env.PORT || 5000;
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 dotenv.config();
 
+const PORT = process.env.PORT || 5000;
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = process.env.MONGO_URI; //aitar modde mongodb ar user pass ase 
+
+// Create MongoClient with options
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect client once at startup
+    await client.connect();
+
+
+    const parcelsCollection = client.db("zapshiftDB").collection("parcels");
+
+    
+    // Ping to confirm connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. Successfully connected to MongoDB!");
+    
+    // Start Express server only after DB connected
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+run();
 
 // Basic route
 app.get('/', (req, res) => {
   res.send('ZAPSHIFT Parcel Delivery Server is running!');
-});
-
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
 });
