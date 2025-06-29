ca94ad4df423edc1f1cc0b81a8e6b4f0730c3840
TUMIT_HASAN <tumithasan1@gmail.com>
2025-06-29 10:51:39 +0600

    create the payment api from the server side

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 
+
+
 dotenv.config();
+
+// payment
+// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+// 
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -37,6 +45,14 @@ async function run() {
             res.send(result);
         });
 
+
+        // for getting data by id
+        app.get('/parcels/:id', async (req, res) => {
+            const id = req.params.id;
+            const parcel = await parcelsCollection.findOne({ _id: new ObjectId(id) });
+            res.send(parcel);
+        });
+
         // for my parcel data & admins data 
         app.get('/parcels', async (req, res) => {
             const email = req.query.email;
@@ -58,12 +74,26 @@ async function run() {
             res.send(result);
         });
 
+        // for payment realted apis 
+        app.post("/create-payment-intent", async (req, res) => {
+            const amount = req.body.amount;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount * 100, // stripe এর amount cent এ হয়
+                currency: "usd",
+                payment_method_types: ["card"],
+            });
+
+            res.send({ clientSecret: paymentIntent.client_secret });
+        });
+
+
 
         app.delete('/parcels/:id', async (req, res) => {
             const id = req.params.id;
             const result = await parcelsCollection.deleteOne({ _id: new ObjectId(id) });
 
-            res.send(result); 
+            res.send(result);
         });
 
         // Ping to confirm connection
