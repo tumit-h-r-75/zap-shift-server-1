75dd8d0a79b2a963c1c17c1e301e116aec1a5a97
TUMIT_HASAN <tumithasan1@gmail.com>
2025-07-02 07:15:06 +0600

    create delete api for delete rider application

--- index.js
+++ index.js
@@ -267,6 +267,15 @@ async function run() {
             res.send(result);
         });
 
+        // for rider data delete
+        app.delete('/riders/:id', async (req, res) => {
+            const id = req.params.id;
+            const result = await ridersCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
+
+
         // Ping to confirm connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. Successfully connected to MongoDB!");
